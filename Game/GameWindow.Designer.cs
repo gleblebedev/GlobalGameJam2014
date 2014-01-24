7be742e37e517198908bba5d6c06649f3413d87e
Gleb Lebedev <glebedev@netvoxlab.ru>
2014-01-24 20:25:50 +0400

    no message

---
+++ Game/GameWindow.Designer.cs
@@ -0,0 +1,65 @@
+﻿using OpenTK;
+
+namespace Game
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(624, 442);
+            this.glControl.TabIndex = 0;
+            this.glControl.VSync = false;
+            this.glControl.Load += new System.EventHandler(this.ControlLoaded);
+            this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.RenderScene);
+            this.glControl.Resize += new System.EventHandler(this.SceneResize);
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.glControl);
+            this.Name = "GameWindow";
+            this.Text = "GGJ2014";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl;
+    }
+}
---
+++ Game/GameWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Game
+{
+    public partial class GameWindow : Form
+    {
+        private bool loaded;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void RenderScene(object sender, PaintEventArgs e)
+        {
+            if (!this.loaded) // Play nice
+            {
+                return;
+            }
+             try
+                        {
+            this.glControl.MakeCurrent();
+            GL.ClearColor(new Color4(0,0x20,0x40,0));
+            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
+            GL.Flush();
+            this.glControl.SwapBuffers();
+                        }
+             catch (Exception ex)
+             {
+                 this.glControl.Visible = false;
+                 MessageBox.Show(ex.ToString(), ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+             }
+        }
+
+        private void SceneResize(object sender, EventArgs e)
+        {
+            if (!this.loaded)
+            {
+                return;
+            }
+            this.glControl.MakeCurrent();
+            this.SetupViewport();
+        }
+
+        private void ControlLoaded(object sender, EventArgs e)
+        {
+            this.loaded = true;
+            this.glControl.MakeCurrent();
+            this.SetupViewport();
+        }
+
+        private void SetupViewport()
+        {
+            if (!this.loaded)
+            {
+                return;
+            }
+            if (GraphicsContext.CurrentContext == null)
+            {
+                return;
+            }
+            int w = Math.Max(1, this.glControl.Width);
+            int h = Math.Max(1, this.glControl.Height);
+            // Use all of the glControl painting area
+            //this.graphicsContext.SetViewport(0, 0, w, h);
+
+            //this.Camera.AspectRatio = w / (float)h;
+        }
+    }
+}
